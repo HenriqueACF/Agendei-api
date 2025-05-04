f6d29fcbd39ca8b48e83066e3194910461407723
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2025-05-04 20:04:07 -0300

    feat: criando SecurityFilter

--- src/main/java/com/appAgendamento/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/appAgendamento/login_auth_api/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.appAgendamento.login_auth_api.repositories;
 import com.appAgendamento.login_auth_api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
